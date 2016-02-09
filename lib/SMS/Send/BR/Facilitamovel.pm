346aacf26633221f1240fb3eca5d187c344bfb55
Mario Celso Teixeira <marioct@cpqd.com.br>
2016-02-09 12:58:38 -0200

    Add Module Pod Doc

--- lib/SMS/Send/BR/Facilitamovel.pm
+++ lib/SMS/Send/BR/Facilitamovel.pm
@@ -70,3 +70,74 @@ sub send_sms {
 }
  
 1;
+
+__END__
+
+=pod
+
+=encoding UTF-8
+
+=head1 NAME
+
+SMS::Send::BR::Facilitamovel - SMS::Send driver for the Facilita Movel service
+
+=head1 VERSION
+
+version 0.03
+
+=head1 SYNOPSIS
+
+    use SMS::Send;
+    my $sender = SMS::Send->new('BR::Facilitamovel',
+        _login    => 'foo',
+        _password => 'bar',
+    );
+
+    my $sent = $sender->send_sms(
+        'text'           => 'This is a test message',
+        'to'             => '19991913030',
+    );
+
+    # Did the send succeed.
+    if ( $sent ) {
+        print "Message sent ok\n";
+    } else {
+        print 'Failed to send message: ', $@->{error_content}, "\n";
+    }
+
+=head1 DESCRIPTION
+
+This module currently uses the L<HTTP API|https://www.facilitamovel.com.br>.
+
+=head1 METHODS
+
+=head2 send_sms
+
+Is called by L<SMS::Send/send_sms> and passes all arguments starting with an
+underscore to the request having the first underscore removed as shown in the
+SYNOPSIS above.
+
+Returns true if the message was successfully sent.
+
+Returns false if an error occurred and $@ is set to a hashref of the following info:
+
+    {
+        statusCode      => "...",
+        MessageId       => "...",
+    }
+
+Throws an exception if a fatal error like a http timeout in the underlying
+connection occurred.
+
+=head1 AUTHOR
+
+Mario Celso Teixeira <marioct37@gmail.com>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) CPqD 2016 by Mario Celso Teixeira.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
